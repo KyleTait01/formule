015c1dcdc6698e51ebe4653ee30c8354c3342168
KyleTait01 <taitk58@gmail.com>
2024-03-12 22:37:32 +0000

    game won screen

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const session = require('express-session');
 
 const app = express();
 app.set("view engine", "ejs");
 
 app.use("/dist", express.static(__dirname + "/dist"));
 app.use(express.urlencoded());
+
+app.use(session({
+	secret: 'your_secret_key',
+	resave: false,
+	saveUninitialized: true,
+}));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
@@ -16,6 +23,18 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get('/win-screen', (req, res) => {
+	const gameWon = req.session.gameWon || false;
+	if (gameWon){
+		res.render('win-screen');
+	}
+})
+
+app.post('/set-game-won', (req, res) => {
+	req.session.gameWon = true;
+	res.sendStatus(200);
+})
 
 // 404 page
 app.use((req, res) => {
